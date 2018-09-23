51a759a54094cfed40e29e5ce1e434d6976a3b49
kotouno yuta <bananakotono@gmail.com>
2018-09-23 21:22:11 +0900

    display log

--- mysite/settings.py
+++ mysite/settings.py
@@ -68,6 +68,32 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}  
+
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
