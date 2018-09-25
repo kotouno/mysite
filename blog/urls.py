021a68f337d707709c318d92b1c8775bc3bf6cc2
kotouno yuta <bananakotono@gmail.com>
2018-09-25 22:35:05 +0900

    create index

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import include, url
+from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list')
+	#url(r'^$', views.post_list, name='post_list')
+    #path('admin/', admin.site.urls),
+    path('', views.index)
 ]
